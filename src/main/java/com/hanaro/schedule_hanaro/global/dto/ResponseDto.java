1603ce7f23f57c8ae4655d54d977843f75b34baf
ImHyungsuk <limhs8630@gmail.com>
2024-12-02 13:26:45 +0900

    [Feat] 🍺 ResponseDto 구현

---
+++ src/main/java/com/hanaro/schedule_hanaro/global/dto/ResponseDto.java
@@ -0,0 +1,29 @@
+package com.hanaro.schedule_hanaro.global.dto;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+
+public record ResponseDto<T>(
+	@JsonIgnore HttpStatus httpStatus,
+	@NotNull boolean success,
+	@Nullable T data
+	) {
+	public static <T> ResponseDto<T> ok(T data){
+		return new ResponseDto<>(
+			HttpStatus.OK,
+			true,
+			data
+		);
+	}
+	public static <T> ResponseDto<T> created(T data){
+		return new ResponseDto<>(
+			HttpStatus.CREATED,
+			true,
+			data
+		);
+	}
+}
