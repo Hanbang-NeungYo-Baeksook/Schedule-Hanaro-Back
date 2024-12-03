7c758afa51310dbdd5397651c65783370eb8fb5e
ImHyungsuk <limhs8630@gmail.com>
2024-12-03 17:16:54 +0900

    [Feat] 🍺 추천답변 엔티티 구현

---
+++ src/main/java/com/hanaro/schedule_hanaro/global/domain/Recommend.java
@@ -0,0 +1,43 @@
+package com.hanaro.schedule_hanaro.global.domain;
+
+import com.hanaro.schedule_hanaro.global.domain.enums.Category;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Recommend {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "recommend_id")
+	private Long id;
+
+	private String query;
+	private String response;
+	private Category category;
+
+	@Column(name = "query_vector")
+	private String queryVector;
+
+	@Builder
+	public Recommend(
+		String query,
+		String response,
+		Category category,
+		String queryVector
+	) {
+		this.query = query;
+		this.response = response;
+		this.category = category;
+		this.queryVector = queryVector;
+	}
+}
