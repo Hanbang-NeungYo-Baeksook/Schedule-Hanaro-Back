d7c2f4a43891c9e3be1cdb111cea96b6f24632ab
LimHyeongseok <limhs8639@gmail.com>
2024-12-13 16:56:09 +0900

    [Style] 🍺 코드 스타일 수정

--- src/main/java/com/hanaro/schedule_hanaro/customer/dto/response/CustomerResponse.java
+++ src/main/java/com/hanaro/schedule_hanaro/customer/dto/response/CustomerResponse.java
@@ -3,7 +3,10 @@ package com.hanaro.schedule_hanaro.customer.dto.response;
 import lombok.Builder;
 
 @Builder
-public record CustomerResponse(String customerName, String phoneNum) {
+public record CustomerResponse(
+		String customerName,
+		String phoneNum
+) {
 	public static CustomerResponse from(
 			final String name,
 			final String phoneNum
