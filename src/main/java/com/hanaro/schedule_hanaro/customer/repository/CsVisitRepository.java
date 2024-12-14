1c9062e52b83889085ad2e02f83c1c5f8dc02953
LimHyeongseok <limhs8639@gmail.com>
2024-12-14 17:35:07 +0900

    [Feat] 🍺 영업점 목록 Repository 기능 구현

--- src/main/java/com/hanaro/schedule_hanaro/customer/repository/CsVisitRepository.java
+++ src/main/java/com/hanaro/schedule_hanaro/customer/repository/CsVisitRepository.java
@@ -1,6 +1,8 @@
 package com.hanaro.schedule_hanaro.customer.repository;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +12,8 @@ import com.hanaro.schedule_hanaro.global.domain.CsVisit;
 @Repository
 public interface CsVisitRepository extends JpaRepository<CsVisit, Integer> {
 
-	 CsVisit findCsVisitByBranchIdAndDate(Long branchId, LocalDate date);
+	Optional<CsVisit> findCsVisitByBranchIdAndDate(Long branchId, LocalDate date);
+
+	List<CsVisit> findAllByDateOrderByBranchAsc(LocalDate date);
+
 }
