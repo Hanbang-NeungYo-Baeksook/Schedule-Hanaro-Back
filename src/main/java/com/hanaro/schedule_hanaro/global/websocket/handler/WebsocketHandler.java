dee3a63293932374df90f87af9641b0b5f0e25fe
happppi <102276917+hhbb0081@users.noreply.github.com>
2024-12-16 01:35:41 +0900

    [Fix] 🍀 관리자 전화상담 대기목록 웹소켓 추가 && 전화상담 등록 시 데이터 전달

--- src/main/java/com/hanaro/schedule_hanaro/global/websocket/handler/WebsocketHandler.java
+++ src/main/java/com/hanaro/schedule_hanaro/global/websocket/handler/WebsocketHandler.java
@@ -1,4 +1,4 @@
-package com.hanaro.schedule_hanaro.global.websocket;
+package com.hanaro.schedule_hanaro.global.websocket.handler;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -35,17 +35,57 @@ public class WebsocketHandler extends TextWebSocketHandler {
 		}
 	}
 
+	// @Override
+	// protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+	// 	String payload = message.getPayload();
+	// 	for (WebSocketSession s : sessions) {
+	// 		s.sendMessage(new TextMessage("Hello!" + payload));
+	// 	}
+	// }
+	//
+	// @Override
+	// public void afterConnectionClosed(WebSocketSession session, CloseStatus status)throws Exception{
+	// 	sessions.remove(session);
+	// }
+
+	////////////////////////////////////
+
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		String payload = message.getPayload();
-		for (WebSocketSession s : sessions) {
-			s.sendMessage(new TextMessage("Hello!" + payload));
+		try {
+			String payload = message.getPayload();
+			Map data = mapper.readValue(payload, Map.class);
+
+			if ("subscribe".equals(data.get("action"))) {
+				Long topicId = Long.valueOf(String.valueOf(data.get("topicId")));
+				scheduleSessionMap.computeIfAbsent(topicId, k -> new HashSet<>()).add(session);
+				session.sendMessage(new TextMessage("구독 완료: " + topicId));
+			} else {
+				session.sendMessage(new TextMessage("알 수 없는 액션: " + data.get("action")));
+			}
+		} catch (Exception e) {
+			session.sendMessage(new TextMessage("잘못된 요청 형식: " + e.getMessage()));
 		}
 	}
 
 	@Override
-	public void afterConnectionClosed(WebSocketSession session, CloseStatus status)throws Exception{
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		sessions.remove(session);
+		// session이 종료된 경우 구독 목록에서 제거
+		scheduleSessionMap.values().forEach(sessions -> sessions.remove(session));
+	}
+
+	// 특정 주제를 구독하는 클라이언트들에게 메시지 전송
+	public void notifySubscribers(Long topicId, String message) {
+		if (scheduleSessionMap.containsKey(topicId)) {
+			for (WebSocketSession session : scheduleSessionMap.get(topicId)) {
+				try {
+					session.sendMessage(new TextMessage(message));
+				} catch (Exception e) {
+					// 에러 처리 (로깅 등)
+				}
+			}
+		}
 	}
 
 }
