3209411cf8c099c15389f37bd21e08d35c343878
LimHyeongseok <limhs8639@gmail.com>
2024-12-15 22:03:00 +0900

    [Feat] 🍺 Admin 테이블 role 컬럼 수정

--- src/main/java/com/hanaro/schedule_hanaro/global/domain/Admin.java
+++ src/main/java/com/hanaro/schedule_hanaro/global/domain/Admin.java
@@ -4,6 +4,8 @@ import com.hanaro.schedule_hanaro.global.domain.enums.Role;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -40,6 +42,7 @@ public class Admin {
 	private Branch branch;
 
 	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
 
 	@Builder
