052eb46cceae64b2916dbb9ea6093ee70271ba20
laputa-n <skatmdgur6365@gmail.com>
2024-12-15 23:06:24 +0900

    [Feat]🌱 관리자 1:1 문의 목록 Controller 구현

--- src/main/java/com/hanaro/schedule_hanaro/admin/repository/AdminInquiryRepository.java
+++ src/main/java/com/hanaro/schedule_hanaro/admin/repository/AdminInquiryRepository.java
@@ -1,17 +1,14 @@
 package com.hanaro.schedule_hanaro.admin.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import com.hanaro.schedule_hanaro.global.domain.Inquiry;
 
-public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+public interface AdminInquiryRepository extends JpaRepository<Inquiry, Long> {
 	@Query("SELECT i FROM Inquiry i " +
 		"LEFT JOIN Customer c ON i.customer.id = c.id " +
 		"WHERE (i.inquiryStatus = :status) " +
@@ -19,7 +16,8 @@ public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
 		"AND (:searchContent IS NULL OR " +
 		"     i.tags LIKE %:searchContent% OR " +
 		"     i.content LIKE %:searchContent% OR " +
-		"     c.name LIKE %:searchContent%)")
+		"     c.name LIKE %:searchContent%)" +
+		"ORDER BY i.createdAt DESC")
 	Page<Inquiry> findFilteredInquiries(
 		@Param("status") String status,
 		@Param("category") String category,
