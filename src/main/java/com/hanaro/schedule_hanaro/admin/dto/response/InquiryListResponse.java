6b0df8c8843705f40537e17a845a098b9e6652a9
laputa-n <skatmdgur6365@gmail.com>
2024-12-15 22:35:30 +0900

    [Feat]🌱 관리자 1:1 문의 목록 InquiryService 구현

--- src/main/java/com/hanaro/schedule_hanaro/admin/dto/response/InquiryListResponse.java
+++ src/main/java/com/hanaro/schedule_hanaro/admin/dto/response/InquiryListResponse.java
@@ -3,7 +3,6 @@ package com.hanaro.schedule_hanaro.admin.dto.response;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.hanaro.schedule_hanaro.customer.dto.response.InquiryResponse;
 import com.hanaro.schedule_hanaro.global.domain.Inquiry;
 
 import lombok.Builder;
@@ -25,7 +24,6 @@ public record InquiryListResponse(
 	@JsonProperty("total_pages")
 	Integer totalPages             // 전체 페이지 수
 ) {
-	// 정적 팩토리 메서드 for DTO 변환
 	public static InquiryListResponse from(List<InquiryData> data, Integer currentPage, Integer pageSize, Long totalItems, Integer totalPages) {
 		return InquiryListResponse.builder()
 			.inquiryList(data)
@@ -59,7 +57,6 @@ public record InquiryListResponse(
 		@JsonProperty("customer_name")
 		String customerName
 	) {
-		// Inquiry 엔티티에서 InquiryData DTO로 변환하는 메서드
 		public static InquiryData from(Inquiry inquiry, String customerName) {
 			return InquiryData.builder()
 				.inquiryId(inquiry.getId())
@@ -68,7 +65,7 @@ public record InquiryListResponse(
 				.content(inquiry.getContent())
 				.tags(inquiry.getTags() != null ? List.of(inquiry.getTags().split(",")) : List.of())
 				.createdAt(inquiry.getCreatedAt().toString())
-				.customerName(customerName) // 고객 이름 전달
+				.customerName(customerName)
 				.build();
 		}
 	}
