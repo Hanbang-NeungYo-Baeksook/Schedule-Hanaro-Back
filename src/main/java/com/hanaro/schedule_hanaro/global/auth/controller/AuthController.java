c4ff539f0972c8c41b269fe7279c7585293305a5
ImHyungsuk <limhs8630@gmail.com>
2024-12-16 13:11:04 +0900

    [Feat] 🍺 Security + jwt 로그인 구현

--- src/main/java/com/hanaro/schedule_hanaro/global/auth/controller/AuthController.java
+++ src/main/java/com/hanaro/schedule_hanaro/global/auth/controller/AuthController.java
@@ -1,36 +1,44 @@
-// package com.hanaro.schedule_hanaro.global.auth.controller;
-//
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-//
-// import com.hanaro.schedule_hanaro.global.auth.dto.request.AuthAdminSignUpRequest;
-// import com.hanaro.schedule_hanaro.global.auth.dto.request.AuthSignUpRequest;
-// import com.hanaro.schedule_hanaro.global.auth.service.AuthService;
-//
-// import lombok.RequiredArgsConstructor;
-// import lombok.extern.slf4j.Slf4j;
-//
-// @Slf4j
-// @RestController
-// @RequestMapping("/api/auth")
-// @RequiredArgsConstructor
-// public class AuthController {
-// 	private final AuthService authService;
-//
-// 	@PostMapping("/sign-up")
-// 	public ResponseEntity<?> signUp(@RequestBody AuthSignUpRequest authSignUpRequest){
-// 		log.info("signUp 컨트롤러 진입");
-// 		authService.signUp(authSignUpRequest);
-// 		return ResponseEntity.ok(null);
-// 	}
-//
-// 	@PostMapping("/admin/sign-up")
-// 	public ResponseEntity<String> postAdmin(@RequestBody AuthAdminSignUpRequest authAdminSignUpRequest) {
-// 		// 관리자 등록 - 테스트용
-// 		return ResponseEntity.ok(authService.adminSignUpAdmin(authAdminSignUpRequest));
-// 	}
-//
-// }
+package com.hanaro.schedule_hanaro.global.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hanaro.schedule_hanaro.global.auth.dto.request.AuthAdminSignUpRequest;
+import com.hanaro.schedule_hanaro.global.auth.dto.request.AuthSignUpRequest;
+import com.hanaro.schedule_hanaro.global.auth.dto.request.SignInRequest;
+import com.hanaro.schedule_hanaro.global.auth.dto.response.JwtTokenDto;
+import com.hanaro.schedule_hanaro.global.auth.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+	private final AuthService authService;
+
+	@PostMapping("/sign-up")
+	public ResponseEntity<?> signUp(@RequestBody AuthSignUpRequest authSignUpRequest){
+		log.info("signUp 컨트롤러 진입");
+		authService.signUp(authSignUpRequest);
+		return ResponseEntity.ok(null);
+	}
+
+	@PostMapping("/sign-in")
+	public ResponseEntity<JwtTokenDto> signIn(@RequestBody SignInRequest signInRequest) {
+		JwtTokenDto response = authService.signIn(signInRequest);
+		return ResponseEntity.ok().body(response);
+	}
+
+	@PostMapping("/admin/sign-up")
+	public ResponseEntity<String> postAdmin(@RequestBody AuthAdminSignUpRequest authAdminSignUpRequest) {
+		// 관리자 등록 - 테스트용
+		return ResponseEntity.ok(authService.adminSignUpAdmin(authAdminSignUpRequest));
+	}
+
+}
--- src/main/java/com/hanaro/schedule_hanaro/global/auth/service/AuthService.java
+++ src/main/java/com/hanaro/schedule_hanaro/global/auth/service/AuthService.java
@@ -1,50 +1,71 @@
-// package com.hanaro.schedule_hanaro.global.auth.service;
-//
-//
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.stereotype.Service;
-//
-// import com.hanaro.schedule_hanaro.admin.repository.AdminRepository;
-// import com.hanaro.schedule_hanaro.customer.repository.BranchRepository;
-// import com.hanaro.schedule_hanaro.global.auth.dto.request.AuthAdminSignUpRequest;
-// import com.hanaro.schedule_hanaro.global.auth.dto.request.AuthSignUpRequest;
-// import com.hanaro.schedule_hanaro.global.domain.Admin;
-// import com.hanaro.schedule_hanaro.global.domain.Branch;
-// import com.hanaro.schedule_hanaro.global.domain.Customer;
-// import com.hanaro.schedule_hanaro.global.domain.enums.Gender;
-// import com.hanaro.schedule_hanaro.customer.repository.CustomerRepository;
-//
-// import lombok.RequiredArgsConstructor;
-//
-// @Service
-// @RequiredArgsConstructor
-// public class AuthService {
-// 	private final CustomerRepository customerRepository;
-// 	private final AdminRepository adminRepository;
-// 	private final BranchRepository branchRepository;
-// 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
-//
-// 	public void signUp(AuthSignUpRequest authSignUpRequest){
-// 		customerRepository.save(Customer.builder()
-// 			.authId(authSignUpRequest.authId())
-// 			.password(bCryptPasswordEncoder.encode(authSignUpRequest.password()))
-// 			.name(authSignUpRequest.name())
-// 			.phoneNum(authSignUpRequest.phoneNum())
-// 			.birth(authSignUpRequest.birth())
-// 			.gender(Gender.valueOf(authSignUpRequest.gender()))
-// 			.build());
-// 	}
-//
-// 	public String adminSignUpAdmin(AuthAdminSignUpRequest authAdminSignUpRequest) {
-// 		Branch branch = branchRepository.findById(1L).orElseThrow();
-// 		adminRepository.save(Admin.builder()
-// 				.authId(authAdminSignUpRequest.authId())
-// 				.password(bCryptPasswordEncoder.encode(authAdminSignUpRequest.password()))
-// 				.name(authAdminSignUpRequest.name())
-// 				.branch(branch)
-// 			.build()
-// 		);
-//
-// 		return "Success";
-// 	}
-// }
+package com.hanaro.schedule_hanaro.global.auth.service;
+
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.hanaro.schedule_hanaro.admin.repository.AdminRepository;
+import com.hanaro.schedule_hanaro.customer.repository.BranchRepository;
+import com.hanaro.schedule_hanaro.global.auth.dto.request.AuthAdminSignUpRequest;
+import com.hanaro.schedule_hanaro.global.auth.dto.request.AuthSignUpRequest;
+import com.hanaro.schedule_hanaro.global.auth.dto.request.SignInRequest;
+import com.hanaro.schedule_hanaro.global.auth.dto.response.JwtTokenDto;
+import com.hanaro.schedule_hanaro.global.auth.info.CustomUserDetails;
+import com.hanaro.schedule_hanaro.global.auth.info.UserInfo;
+import com.hanaro.schedule_hanaro.global.auth.provider.JwtAuthenticationProvider;
+import com.hanaro.schedule_hanaro.global.auth.provider.JwtTokenProvider;
+import com.hanaro.schedule_hanaro.global.domain.Admin;
+import com.hanaro.schedule_hanaro.global.domain.Branch;
+import com.hanaro.schedule_hanaro.global.domain.Customer;
+import com.hanaro.schedule_hanaro.global.domain.enums.Gender;
+import com.hanaro.schedule_hanaro.customer.repository.CustomerRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+	private final CustomerRepository customerRepository;
+	private final AdminRepository adminRepository;
+	private final BranchRepository branchRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final JwtAuthenticationProvider authenticationProvider;
+	private final JwtTokenProvider jwtTokenProvider;
+
+
+	public JwtTokenDto signIn(SignInRequest signInRequest) {
+		String username = signInRequest.authId();
+		String password = signInRequest.password();
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+			password);
+		Authentication authentication = authenticationProvider.authenticate(authenticationToken);
+		CustomUserDetails customUserDetails = (CustomUserDetails)authentication.getPrincipal();
+		return jwtTokenProvider.generateTokens(customUserDetails.getUsername(), customUserDetails.getRole());
+	}
+
+	public void signUp(AuthSignUpRequest authSignUpRequest){
+		customerRepository.save(Customer.builder()
+			.authId(authSignUpRequest.authId())
+			.password(bCryptPasswordEncoder.encode(authSignUpRequest.password()))
+			.name(authSignUpRequest.name())
+			.phoneNum(authSignUpRequest.phoneNum())
+			.birth(authSignUpRequest.birth())
+			.gender(Gender.valueOf(authSignUpRequest.gender()))
+			.build());
+	}
+
+	public String adminSignUpAdmin(AuthAdminSignUpRequest authAdminSignUpRequest) {
+		Branch branch = branchRepository.findById(1L).orElseThrow();
+		adminRepository.save(Admin.builder()
+				.authId(authAdminSignUpRequest.authId())
+				.password(bCryptPasswordEncoder.encode(authAdminSignUpRequest.password()))
+				.name(authAdminSignUpRequest.name())
+				.branch(branch)
+			.build()
+		);
+
+		return "Success";
+	}
+}
