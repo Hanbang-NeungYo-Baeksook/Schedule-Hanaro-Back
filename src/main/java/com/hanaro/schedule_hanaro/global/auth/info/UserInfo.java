485c9c46d45f26bc6ae3948392593f9f399fcc9c
ImHyungsuk <limhs8630@gmail.com>
2024-12-16 14:14:04 +0900

    Conflict 해결

--- src/main/java/com/hanaro/schedule_hanaro/global/auth/info/UserInfo.java
+++ src/main/java/com/hanaro/schedule_hanaro/global/auth/info/UserInfo.java
@@ -1,9 +1,9 @@
-// package com.hanaro.schedule_hanaro.global.auth.info;
-//
-// import com.hanaro.schedule_hanaro.global.domain.enums.Role;
-//
-// public record UserInfo(
-// 	String id,
-// 	Role role
-// ) {
-// }
+package com.hanaro.schedule_hanaro.global.auth.info;
+
+import com.hanaro.schedule_hanaro.global.domain.enums.Role;
+
+public record UserInfo(
+	String id,
+	Role role
+) {
+}
