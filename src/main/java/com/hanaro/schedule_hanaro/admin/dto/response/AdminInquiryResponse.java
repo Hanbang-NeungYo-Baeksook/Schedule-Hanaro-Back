8abcfaf9814982cf18c46d691b06f2cbed0f1d16
laputa-n <skatmdgur6365@gmail.com>
2024-12-16 10:43:05 +0900

    [Feat]🌱 관리자 1:1 문의 답변 등록 dto, service 구현

---
+++ src/main/java/com/hanaro/schedule_hanaro/admin/dto/response/AdminInquiryResponse.java
@@ -0,0 +1,24 @@
+package com.hanaro.schedule_hanaro.admin.dto.response;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+
+@Builder
+public record AdminInquiryResponse(
+	@JsonProperty("inquiry_id") Long inquiryId,
+	@JsonProperty("admin_id") Long adminId,
+	@JsonProperty("content") String content,
+	@JsonProperty("created_at") LocalDateTime createdAt
+) {
+	public static AdminInquiryResponse from(Long inquiryId, Long adminId, String content, LocalDateTime createdAt) {
+		return AdminInquiryResponse.builder()
+			.inquiryId(inquiryId)
+			.adminId(adminId)
+			.content(content)
+			.createdAt(createdAt)
+			.build();
+	}
+}
--- src/main/java/com/hanaro/schedule_hanaro/admin/repository/AdminRepository.java
+++ src/main/java/com/hanaro/schedule_hanaro/admin/repository/AdminRepository.java
@@ -1,8 +1,12 @@
 package com.hanaro.schedule_hanaro.admin.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hanaro.schedule_hanaro.global.domain.Admin;
 
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+	@Override
+	Optional<Admin> findById(Long aLong);
 }
