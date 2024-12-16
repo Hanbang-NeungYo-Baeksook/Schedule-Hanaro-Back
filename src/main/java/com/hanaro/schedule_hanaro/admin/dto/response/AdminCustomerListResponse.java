d27e615547b7e0a084fedcbfaf3f9a6589071318
laputa-n <skatmdgur6365@gmail.com>
2024-12-16 11:18:34 +0900

    [Feat]🌱 관리자 고객 목록 dto 구현

---
+++ src/main/java/com/hanaro/schedule_hanaro/admin/dto/response/AdminCustomerListResponse.java
@@ -0,0 +1,46 @@
+package com.hanaro.schedule_hanaro.admin.dto.response;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.hanaro.schedule_hanaro.global.domain.Customer;
+
+import lombok.Builder;
+
+@Builder
+public record AdminCustomerListResponse(
+	List<CustomerData> customers,
+	@JsonProperty("current_page") Integer currentPage,
+	@JsonProperty("page_size") Integer pageSize,
+	@JsonProperty("total_items") Long totalItems,
+	@JsonProperty("total_pages") Integer totalPages
+) {
+
+	@Builder
+	public record CustomerData(
+		@JsonProperty("customer_name") String customerName,
+		@JsonProperty("phone_number") String phoneNumber,
+		@JsonProperty("birth_date") String birthDate,
+		String email
+	) {
+		public static CustomerData from(Customer customer) {
+			return CustomerData.builder()
+				.customerName(customer.getName())
+				.phoneNumber(customer.getPhoneNum())
+				.birthDate(customer.getBirth().toString())
+				.email(customer.getAuthId())
+				.build();
+		}
+	}
+
+	public static AdminCustomerListResponse from(
+		List<CustomerData> customers, Integer currentPage, Integer pageSize, Long totalItems, Integer totalPages) {
+		return AdminCustomerListResponse.builder()
+			.customers(customers)
+			.currentPage(currentPage)
+			.pageSize(pageSize)
+			.totalItems(totalItems)
+			.totalPages(totalPages)
+			.build();
+	}
+}
