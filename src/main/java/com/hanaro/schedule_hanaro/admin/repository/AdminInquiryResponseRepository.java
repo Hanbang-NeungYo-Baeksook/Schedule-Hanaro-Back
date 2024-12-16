92b137505eea3e47dc492c13f752d0bf7f59c792
laputa-n <skatmdgur6365@gmail.com>
2024-12-16 09:19:47 +0900

    [Feat]🌱 관리자 1:1 문의 상세 리뷰 반영

--- src/main/java/com/hanaro/schedule_hanaro/admin/repository/AdminInquiryResponseRepository.java
+++ src/main/java/com/hanaro/schedule_hanaro/admin/repository/AdminInquiryResponseRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface AdminInquiryResponseRepository extends JpaRepository<InquiryResponse, Long> {
-	@Query("SELECT ir FROM InquiryResponse ir WHERE ir.inquiry.id = :inquiryId")
+	// @Query("SELECT ir FROM InquiryResponse ir WHERE ir.inquiry.id = :inquiryId")
 	Optional<InquiryResponse> findByInquiryId(@Param("inquiryId") Long inquiryId);
 }
