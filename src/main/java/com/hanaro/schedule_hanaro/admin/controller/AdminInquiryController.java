67522e149cc20869d91d8e1d29e7eaec90aa6365
laputa-n <skatmdgur6365@gmail.com>
2024-12-16 10:53:14 +0900

    [Feat]🌱 관리자 1:1 문의 답변 등록 controller 구현

--- src/main/java/com/hanaro/schedule_hanaro/admin/controller/AdminInquiryController.java
+++ src/main/java/com/hanaro/schedule_hanaro/admin/controller/AdminInquiryController.java
@@ -4,13 +4,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hanaro.schedule_hanaro.admin.dto.request.AdminInquiryListRequest;
+import com.hanaro.schedule_hanaro.admin.dto.request.AdminInquiryResponseRequest;
 import com.hanaro.schedule_hanaro.admin.dto.response.AdminInquiryDetailResponse;
 import com.hanaro.schedule_hanaro.admin.dto.response.AdminInquiryListResponse;
+import com.hanaro.schedule_hanaro.admin.dto.response.AdminInquiryResponse;
 import com.hanaro.schedule_hanaro.admin.service.AdminInquiryService;
 import com.hanaro.schedule_hanaro.global.domain.enums.Category;
 
@@ -42,4 +45,12 @@ public class AdminInquiryController {
 		return ResponseEntity.ok().body(response);
 	}
 
+	@PostMapping("/register/{inquiry-id}")
+	public ResponseEntity<AdminInquiryResponse> registerInquiryResponse(
+		@PathVariable("inquiry-id") Long inquiryId,
+		@RequestBody AdminInquiryResponseRequest request
+	) {
+		AdminInquiryResponse response = adminInquiryService.registerInquiryResponse(inquiryId, request);
+		return ResponseEntity.status(201).body(response);
+	}
 }
