e2939463ef5eb0287601e9e56fbb858c85ee1f37
laputa-n <skatmdgur6365@gmail.com>
2024-12-16 11:38:29 +0900

    [Feat]🌱 관리자 고객 목록 API service, controller 구현

--- src/main/java/com/hanaro/schedule_hanaro/admin/service/AdminCustomerService.java
+++ src/main/java/com/hanaro/schedule_hanaro/admin/service/AdminCustomerService.java
@@ -1,10 +1,15 @@
 package com.hanaro.schedule_hanaro.admin.service;
 
+import com.hanaro.schedule_hanaro.admin.dto.response.AdminCustomerListResponse;
 import com.hanaro.schedule_hanaro.customer.repository.CallRepository;
 import com.hanaro.schedule_hanaro.customer.repository.InquiryRepository;
 import com.hanaro.schedule_hanaro.global.domain.Call;
 import com.hanaro.schedule_hanaro.global.domain.Inquiry;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.hanaro.schedule_hanaro.admin.dto.response.AdminCallDto;
@@ -60,6 +65,22 @@ public class AdminCustomerService {
                 .collect(Collectors.toList());
 
         return AdminCustomerInquiryListResponse.of(callDtos, inquiryDtos);
+    }
+
+    public AdminCustomerListResponse getCustomerList(int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        Page<Customer> customers = customerRepository.findAll(pageable);
+
+        return AdminCustomerListResponse.from(
+            customers.getContent().stream()
+                .map(AdminCustomerListResponse.CustomerData::of)
+                .toList(),
+            customers.getNumber() + 1,
+            customers.getSize(),
+            customers.getTotalElements(),
+            customers.getTotalPages()
+        );
     }
 
 }
