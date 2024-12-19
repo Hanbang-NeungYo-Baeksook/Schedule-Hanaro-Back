051e3b6f56a6acb755d26496409209da697951ab
ImHyungsuk <limhs8630@gmail.com>
2024-12-19 09:37:01 +0900

    [Feat] 🍺 카테고리별 상담 시간 임시 등록

--- src/main/java/com/hanaro/schedule_hanaro/global/domain/enums/Category.java
+++ src/main/java/com/hanaro/schedule_hanaro/global/domain/enums/Category.java
@@ -6,21 +6,22 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Category {
-	SIGNIN("로그인"),
-	SIGNUP("회원가입"),
-	AUTH("인증"),
-	DEPOSIT("예금"),
-	TRUST("신탁"),
-	FUND("펀드"),
-	LOAN("대출"),
-	FOREX("외환"),
-	INERNET_BANKING("인터넷뱅킹"),
-	HANAONEQ("하나원큐"),
-	PHONE_BANKING("폰뱅킹"),
-	CD("CD"),
-	ATM("ATM"),
-	UTILITY_BILL("공과금납부"),
-	FOREIGN("해외"),
-	BRANCH("영업점");
+	SIGNIN("로그인",1),
+	SIGNUP("회원가입",1),
+	AUTH("인증",3),
+	DEPOSIT("예금",5),
+	TRUST("신탁",6),
+	FUND("펀드",7),
+	LOAN("대출",30),
+	FOREX("외환",20),
+	INERNET_BANKING("인터넷뱅킹",13),
+	HANAONEQ("하나원큐",7),
+	PHONE_BANKING("폰뱅킹",6),
+	CD("CD",14),
+	ATM("ATM",45),
+	UTILITY_BILL("공과금납부",23),
+	FOREIGN("해외",25),
+	BRANCH("영업점",20);
 	private final String category;
+	private final int waitTime;
 }
