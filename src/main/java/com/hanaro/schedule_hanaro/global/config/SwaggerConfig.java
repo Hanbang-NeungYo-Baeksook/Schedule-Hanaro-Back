7e02aa44b879ccf8f818833a2f584b10b2093411
ImHyungsuk <limhs8630@gmail.com>
2024-12-20 14:55:20 +0900

    [Feat] 🍺 Swagger jwt 로그인 구현

--- src/main/java/com/hanaro/schedule_hanaro/global/config/SwaggerConfig.java
+++ src/main/java/com/hanaro/schedule_hanaro/global/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.hanaro.schedule_hanaro.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +14,26 @@ public class SwaggerConfig {
 
 	@Bean
 	public OpenAPI customOpenAPI() {
+		String jwt = "JWT";
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+		Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+			.name(jwt)
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT")
+		);
+
 		return new OpenAPI()
-			.info(new Info()
-				.title("Schedule 하나路 API")
-				.version("1.0")
-				.description("전체 API 명세서"));
+			.components(new Components())
+			.info(apiInfo())
+			.addSecurityItem(securityRequirement)
+			.components(components);
+	}
+
+	private Info apiInfo() {
+		return new Info()
+			.title("Schedule 하나路 API")
+			.version("1.0")
+			.description("전체 API 명세서");
 	}
 }
