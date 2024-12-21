0713dd1e5fdcd2877d9979c9f8fe70589d065f09
teadmu <dharmavarsa@naver.com>
2024-12-21 21:40:53 +0900

    [Fix]🥚관리자 정보 반환 쿼리 수정

--- src/main/java/com/hanaro/schedule_hanaro/admin/service/AdminInfoService.java
+++ src/main/java/com/hanaro/schedule_hanaro/admin/service/AdminInfoService.java
@@ -6,6 +6,7 @@ import com.hanaro.schedule_hanaro.admin.dto.response.AdminInquiryStatsDto;
 import com.hanaro.schedule_hanaro.global.exception.ErrorCode;
 import com.hanaro.schedule_hanaro.global.exception.GlobalException;
 import com.hanaro.schedule_hanaro.global.repository.AdminRepository;
+import com.hanaro.schedule_hanaro.global.repository.CallRepository;
 import com.hanaro.schedule_hanaro.global.repository.InquiryRepository;
 import com.hanaro.schedule_hanaro.global.domain.Admin;
 import com.hanaro.schedule_hanaro.global.utils.PrincipalUtils;
@@ -22,14 +23,15 @@ import org.springframework.stereotype.Service;
 public class AdminInfoService {
     private final AdminRepository adminRepository;
     private final InquiryRepository inquiryRepository;
+    private final CallRepository callRepository;
 
     public AdminInfoResponse getAdminStats(Authentication authentication) {
         Long id = PrincipalUtils.getId(authentication);
         Admin admin = adminRepository.findById(id)
             .orElseThrow(() -> new GlobalException(ErrorCode.NOT_FOUND_ADMIN));
 
-        AdminInquiryStatsDto phoneInquiryStats = inquiryRepository.findStatsByAdminId(admin.getId());
-        AdminInquiryStatsDto oneToOneInquiryStats = inquiryRepository.findStatsByAdminId(admin.getId());
+        AdminInquiryStatsDto phoneInquiryStats = callRepository.getStatsByAdminId(admin.getId());
+        AdminInquiryStatsDto oneToOneInquiryStats = inquiryRepository.getStatsByAdminId(admin.getId());
 
         return AdminInfoResponse.from(admin ,phoneInquiryStats, oneToOneInquiryStats);
     }
