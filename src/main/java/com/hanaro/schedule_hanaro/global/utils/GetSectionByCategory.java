4751ae9ec019e2a4d9963cc0654613f8914efae8
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-12-21 19:53:34 +0900

    [Refactor] 🐯 Service 메소드 -> Utils로 분리

---
+++ src/main/java/com/hanaro/schedule_hanaro/global/utils/GetSectionByCategory.java
@@ -0,0 +1,20 @@
+package com.hanaro.schedule_hanaro.global.utils;
+
+import com.hanaro.schedule_hanaro.global.domain.enums.Category;
+import com.hanaro.schedule_hanaro.global.domain.enums.SectionType;
+
+public class GetSectionByCategory {
+	public static SectionType getSectionTypeByCategory(Category category) {
+		switch (category) {
+			case FUND, DEPOSIT -> {
+				return SectionType.DEPOSIT;
+			}
+			case FOREX -> {
+				return SectionType.PERSONAL_LOAN;
+			}
+			default -> {
+				return SectionType.BUSINESS_LOAN;
+			}
+		}
+	}
+}
