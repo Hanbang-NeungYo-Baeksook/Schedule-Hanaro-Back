52e66aac87ff1e0e7d3d97743867dc345bab2317
GyuhoTiger <gyuhotiger0303@gmail.com>
2024-12-21 14:09:10 +0900

    [Feat] 🐯 추천 영업점 알고리즘 구현 및 수정

--- src/main/java/com/hanaro/schedule_hanaro/global/repository/BranchRepository.java
+++ src/main/java/com/hanaro/schedule_hanaro/global/repository/BranchRepository.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.hanaro.schedule_hanaro.global.domain.Branch;
 import com.hanaro.schedule_hanaro.global.domain.enums.BranchType;
+import com.hanaro.schedule_hanaro.global.domain.Section;
 
 public interface BranchRepository extends JpaRepository<Branch, Long> {
 	@Override
