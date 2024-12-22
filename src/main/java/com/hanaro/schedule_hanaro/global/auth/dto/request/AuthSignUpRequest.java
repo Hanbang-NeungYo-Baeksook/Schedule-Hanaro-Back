271521d0fc45370e60dffebc1dcd3081985edd76
laputa-n <skatmdgur6365@gmail.com>
2024-12-22 22:39:44 +0900

    [Feat] 🌱 로그인/회원가입 test 코드 작성

--- src/main/java/com/hanaro/schedule_hanaro/global/auth/dto/request/AuthSignUpRequest.java
+++ src/main/java/com/hanaro/schedule_hanaro/global/auth/dto/request/AuthSignUpRequest.java
@@ -1,7 +1,12 @@
 package com.hanaro.schedule_hanaro.global.auth.dto.request;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import java.time.LocalDate;
 
+@Getter
+@Builder
 public record AuthSignUpRequest(
 
 	String authId,
