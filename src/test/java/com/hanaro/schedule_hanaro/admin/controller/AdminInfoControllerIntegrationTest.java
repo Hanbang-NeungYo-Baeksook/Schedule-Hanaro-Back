60011168dd702fbdf24ebea63774658825a3d9b3
teadmu <dharmavarsa@naver.com>
2024-12-23 10:41:04 +0900

    [Feat]🥚관리자 정보 테스트코드

---
+++ src/test/java/com/hanaro/schedule_hanaro/admin/controller/AdminInfoControllerIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.hanaro.schedule_hanaro.admin.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminInfoControllerIntegrationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAdminStats() {
+    }
+}
