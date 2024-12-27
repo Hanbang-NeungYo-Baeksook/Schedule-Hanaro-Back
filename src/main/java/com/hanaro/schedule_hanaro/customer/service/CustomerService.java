1cb9225c411bdb2fb23aa7512f43c8dfb046e20c
ImHyungsuk <limhs8630@gmail.com>
2024-12-27 17:56:47 +0900

    [Hotfix] 🍺 고객정보에서 전화상담 내역 조건 수정

--- src/main/java/com/hanaro/schedule_hanaro/customer/service/CustomerService.java
+++ src/main/java/com/hanaro/schedule_hanaro/customer/service/CustomerService.java
@@ -6,10 +6,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.hanaro.schedule_hanaro.customer.dto.response.CustomerInfoResponse;
+import com.hanaro.schedule_hanaro.global.domain.Customer;
 import com.hanaro.schedule_hanaro.global.domain.enums.Status;
 import com.hanaro.schedule_hanaro.global.repository.CallRepository;
 import com.hanaro.schedule_hanaro.global.repository.CustomerRepository;
-import com.hanaro.schedule_hanaro.global.domain.Customer;
 import com.hanaro.schedule_hanaro.global.repository.InquiryRepository;
 import com.hanaro.schedule_hanaro.global.repository.VisitRepository;
 import com.hanaro.schedule_hanaro.global.utils.PrincipalUtils;
@@ -23,15 +23,15 @@ public class CustomerService {
 	private final VisitRepository visitRepository;
 	private final CallRepository callRepository;
 	private final InquiryRepository inquiryRepository;
-	
+
 	public CustomerInfoResponse findCustomer(Authentication authentication) {
 		Long id = PrincipalUtils.getId(authentication);
 		Customer customer = customerRepository.findById(id).orElseThrow();
-		Integer callAmount=callRepository.countCallsByCustomer(customer);
+		Integer callAmount = callRepository.countCallsByCustomerAndStatusNotIn(customer, Status.CANCELED);
 		Integer inquiryAmount = inquiryRepository.countInquiryByCustomer(customer);
 		Integer visitAmount = visitRepository.countByCustomerAndVisitDateAndStatus(customer, LocalDate.now(),
 			Status.PENDING);
 		return CustomerInfoResponse.of(customer.getName(), customer.getAuthId(), customer.getBirth().toString(),
-			customer.getPhoneNum(), callAmount, inquiryAmount,visitAmount);
+			customer.getPhoneNum(), callAmount, inquiryAmount, visitAmount);
 	}
 }
